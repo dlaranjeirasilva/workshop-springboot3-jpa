8b64d9f86770fde9a9cd67d38a402aaa94016051
dlaranjeirasilva <doug.ark@gmail.com>
2022-12-21 12:38:14 -0300

    Category entity-repo-service

---
+++ src/main/java/com/omnidlaranjeira/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.omnidlaranjeira.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.omnidlaranjeira.course.entities.Category;
+import com.omnidlaranjeira.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
